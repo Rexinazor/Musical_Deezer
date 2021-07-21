46ba91ebd0c15772c020d56fb2f20258143df147
Ｒｅｘｉｎａｚｏｒ <85327329+Rexinazor@users.noreply.github.com>
2021-07-21 20:36:07 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,20 @@
+![Slam](https://telegra.ph/file/5b0ae1361fd50dad69e4a.jpg)
+# Deegram
+![GitHub Repo stars](https://img.shields.io/github/stars/breakdowns/deegram)
+![GitHub forks](https://img.shields.io/github/forks/breakdowns/deegram)
+![GitHub issues](https://img.shields.io/github/issues/breakdowns/deegram)
+![GitHub pull requests](https://img.shields.io/github/issues-pr/breakdowns/deegram)
+![GitHub contributors](https://img.shields.io/github/contributors/breakdowns/deegram)
+![GitHub repo size](https://img.shields.io/github/repo-size/breakdowns/deegram)
+![GitHub commit activity](https://img.shields.io/github/commit-activity/m/breakdowns/deegram)
+
+> Telegram Bot to download FLAC HIFI high quality music from Deezer powered by [deethon](https://github.com/deethon)
+> Watch our videos on How to deploy various useful telegram bots on heroku
+> <p><a href="https://www.youtube.com/channel/UCigq8CVltGNAM-hs6cAC8mw"> <img src="https://telegra.ph/file/c54751325802bfa2313b9.jpg" width="220""/></a></p>
+  
+# Deploy
+<p><a href="https://heroku.com/deploy"> <img src="https://img.shields.io/badge/Deploy%20To%20Heroku-blueviolet?style=for-the-badge&logo=heroku" width="200""/></a></p>
+
+# Credits
+- [aykxt](https://github.com/aykxt) for deethon
+- [Lonami](https://github.com/Lonami) for telethon
---
+++ deegram/utils/translate.py
@@ -0,0 +1,52 @@
+VERSION = "0.2.4"
+CREATOR = "@finite_xd"
+BOT_NAME = "FINITE MUSIC"
+YOUTUBE = "https://www.youtube.com/channel/UCigq8CVltGNAM-hs6cAC8mw"
+WELCOME_MSG = (
+    f"🎶🎵 Welcome to {BOT_NAME} 🎵🎶\n\n"
+    "Click /help for more information\n\n"
+    "join @finiteyt for more bots"
+)
+INFO_MSG = (
+    f"👤 Creator: {CREATOR}\n"
+    f"ℹ Version: {VERSION}\n"
+    f"🍩 Youtube: {YOUTUBE}"
+)
+STATS_MSG = (
+    "**Bot Uptime:** {}\n"
+    "**Total Disk Space:** {}\n"
+    "**Used:** {} "
+    "**Free:** {}\n\n"
+    "📊Data Usage📊\n**Upload:** {}\n"
+    "**Download:** {}\n\n"
+    "**CPU:** {}\n"
+    "**RAM:** {}\n"
+    "**DISK:** {}"
+)
+HELP_MSG = (
+    "Search by Album or Track or just send me a Deezer Track or Album link and I will download it for you bot by @finite_xd\n\n"
+    "**List of all commands:**\n"
+    "/start - Get the welcome message\n"
+    "/help - Get this message\n"
+    "/settings - Change your preferences\n"
+    "/info - Get some useful information about the Bot\n"
+    "/stats - Get some statistics about the Bot\n"
+)
+DOWNLOAD_MSG = "Downloading…"
+UPLOAD_MSG = "Uploading…"
+END_MSG = "Finished."
+ALBUM_MSG = (
+    "💽 Album: {}\n"
+    "👤 Artist: {}\n"
+    "📅 Date: {}\n"
+    "🎧 Total tracks: {}"
+)
+TRACK_MSG = (
+    "🎧 Track: {}\n"
+    "👤 Artist: {}\n"
+    "💽 Album: {}\n"
+    "📅 Date: {}"
+)
+CHOOSE = "Choose:"
+SEARCH_ALBUM = "Search Album 💽"
+SEARCH_TRACK = "Search Track 🎧"
